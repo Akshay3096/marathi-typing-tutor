b0e82ba09701b55949665116c106b4d892f4d6d2
Akshay Yewale <akshay.yewale007@gmail.com>
2026-08-05 21:51:05 +0530

    feat: add reusable stat card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Layout from "./components/Layout/Layout";
+import StatCard from "./components/StatCard/StatCard";
 
 function App() {
   return (
@@ -8,8 +9,12 @@ function App() {
 
       <Layout>
 
-      </Layout>
+        <StatCard
+          title="WPM"
+          value="0"
+        />
 
+      </Layout>
     </>
   );
 }
