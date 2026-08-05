428ee7ab1a53cb8aa54e5f0156268ff1ec78a821
Akshay Yewale <akshay.yewale007@gmail.com>
2026-08-05 21:40:03 +0530

    Initial React + Vite setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-container">
+
+                <div className="logo">
+
+                    ⌨️ Marathi Typing Tutor
+
+                </div>
+
+                <div className="version">
+
+                    Version 1.0
+
+                </div>
+
+            </div>
+        </header>
+    );
+}
+
+export default Header;
