66006b9cd167dba747153732528ffc612f309927
Akshay Yewale <akshay.yewale007@gmail.com>
2026-08-06 20:53:27 +0530

    feat: create typing hook

--- src/hooks/useTyping.js
+++ src/hooks/useTyping.js
@@ -1,3 +1,38 @@
-export function useTyping() {
+import { useState } from "react";
+import { paragraphs } from "../data/paragraphs";
 
+export function useTyping() {
+  const [currentParagraphIndex, setCurrentParagraphIndex] = useState(0);
+
+  const [typedText, setTypedText] = useState("");
+
+  const currentParagraph = paragraphs[currentParagraphIndex];
+
+  const currentIndex = typedText.length;
+
+  const handleTyping = (value) => {
+    setTypedText(value);
+  };
+
+  const restartTyping = () => {
+    setTypedText("");
+  };
+
+  const nextParagraph = () => {
+    const nextIndex =
+      (currentParagraphIndex + 1) % paragraphs.length;
+
+    setCurrentParagraphIndex(nextIndex);
+
+    setTypedText("");
+  };
+
+  return {
+    currentParagraph,
+    typedText,
+    currentIndex,
+    handleTyping,
+    restartTyping,
+    nextParagraph,
+  };
 }
